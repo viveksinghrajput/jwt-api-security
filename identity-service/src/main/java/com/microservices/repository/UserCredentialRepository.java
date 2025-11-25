053e42cd8d5e86160a85062b124585ee403aa2cb
vivek <viveksingh6262@rediffmail.com>
2025-11-25 04:07:48 -0800

    jwt security impl for microservices

---
+++ identity-service/src/main/java/com/microservices/repository/UserCredentialRepository.java
@@ -0,0 +1,10 @@
+package com.microservices.repository;
+
+import com.microservices.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserCredentialRepository  extends JpaRepository<UserCredential,Integer> {
+    Optional<UserCredential> findByName(String username);
+}
---
+++ swiggy-app/src/main/java/com/microservices/client/RestaurantServiceClient.java
@@ -0,0 +1,26 @@
+package com.microservices.client;
+
+import com.microservices.dto.OrderResponseDTO;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.sql.Date;
+
+@Component
+public class RestaurantServiceClient {
+    @Autowired
+    private RestTemplate template;
+
+    private final String SWIGGI_APP="swiggiApp";
+
+    @Retry(name =SWIGGI_APP,fallbackMethod = "fallbackForApiCall")
+    public OrderResponseDTO fetchOrderStatus(String orderId) {
+        return template.getForObject("http://RESTAURANT-SERVICE/restaurant/orders/status/" + orderId, OrderResponseDTO.class);
+    }
+    public OrderResponseDTO fallbackForApiCall(Exception t) {
+        System.out.println("Fallback method executed due to: " + t.getMessage());
+        return new OrderResponseDTO("123","test",2,20.0,new Date(System.currentTimeMillis()),"aprroved",1);
+    }
+}
